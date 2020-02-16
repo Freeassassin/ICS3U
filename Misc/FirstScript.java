8867ea1117b0ba87c3dab44b814c6aa887088842
Freeassassin(LapTop) <farbodemzadeh@gmail.com>
2020-02-15 22:27:49 -0500

    2020-02-15

---
+++ Misc/FirstScript.java
@@ -0,0 +1,11 @@
+public class FirstScript
+{
+    public static void main(String[] args) 
+    {
+		System.out.println("**********************************************************************************************************\n***Data types in Java																				   ***\n**********************************************************************************************************\nPrimitive     	Type/Storage Size   		Values\nboolean    	        1 bit   	        	Logical value of true or false\nchar              	16 bits     	        Stores any of 65436 single chars of UNICODE set\nbyte              	8 bits   	   	        Signed whole numbers –128 to + 127\nshort             	16 bits                 Signed whole numbers –32768 to +32768");
+		System.out.print("\n\n\n");
+		System.out.print("Hello\n\nBuenos Dias\n\nBonjour\n\n\n");
+		System.out.println("*************************************************************\n***Score Board Record                          	          ***\n*************************************************************\nTeam                       	    	Wins    	Losses\n   Jaguars                          10          5\n   Cheetahs                         14          1\n   Panthers                         8           7\n   Penguins 	                    4           11");
+ 
+    }
+}
---
+++ Misc/MohammadzadehF_FirstScript.java
@@ -0,0 +1,26 @@
+/*
+First Script v 1.0
+Farbod Mohammadzadeh
+2020-02-13
+Mr. Rosenberg ICS3U1
+This is a program to shoowcase the print fucntion of the java language
+*/
+
+
+//main class
+public class MohammadzadehF_FirstScript
+{
+	// main method
+    public static void main(String[] args) 
+    {
+    	// print the datatypes script
+		System.out.println("**********************************************************************************************************\n***Data types in Java\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t   ***\n**********************************************************************************************************\nPrimitive\t\tType/Storage Size\t\tValues\nboolean\t\t\t\t1 bit\t\t\t\tLogical value of true or false\nchar\t\t\t\t16 bits\t\t\t\tStores any of 65436 single chars of UNICODE set\nbyte\t\t\t\t8 bits\t\t\t\tSigned whole numbers –128 to + 127\nshort\t\t\t\t16 bits\t\t\t\tSigned whole numbers –32768 to +32768");
+		//give space betwween blocks of text
+		System.out.print("\n\n\n");
+		//print the hello in different languages
+		System.out.print("Hello,\n\nBuenos Dias,\n\nBonjour\n\n\n\n");
+		//print the score board
+		System.out.println("*************************************************************\n***Score Board Record\t\t\t\t\t\t\t\t\t  ***\n*************************************************************\nTeam\t\t\t\t\t\t\t\tWins\t\tLosses\n\tJaguars\t\t\t\t\t\t\t10\t\t\t5\n\tCheetahs\t\t\t\t\t\t14\t\t\t1\n\tPanthers\t\t\t\t\t\t8\t\t\t7\n\tPenguins\t\t\t\t\t\t4\t\t\t11");
+ 
+    }
+}
